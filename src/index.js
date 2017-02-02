88edd72676197e52cec5d50f0dc58edb1efb87cb
Chris Cowan <agentme49@gmail.com>
2017-02-02 14:10:56 -0800

    Initial working version.

---
+++ src/index.js
@@ -0,0 +1,222 @@
+/* @flow */
+
+import asap from 'asap';
+import $$observable from 'symbol-observable';
+
+export type LiveSetChangeRecord<T> =
+  {type: 'add', value: T} |
+  {type: 'remove', value: T} |
+  {type: 'end'};
+
+export type LiveSetController<T> = {
+  closed: boolean;
+  add(item: T): void;
+  remove(item: T): void;
+  error(err: any): void;
+  end(): void;
+};
+
+export type LiveSetInit<T> = {
+  read(): Set<T>;
+  listen(controller: LiveSetController<T>): ?{unsubscribe():void}|()=>void;
+};
+
+export type LiveSetSubscriber<T> = (changes: Array<LiveSetChangeRecord<T>>) => void;
+
+export type LiveSetSubscription = {
+  closed: boolean;
+  unsubscribe(): void;
+};
+
+export type LiveSetObserver<T> = {
+  start?: ?(subscription: LiveSetSubscription) => void;
+  next?: ?(changes: Array<LiveSetChangeRecord<T>>) => void;
+  error?: ?(err: any) => void;
+  complete?: ?() => void;
+};
+
+export default class LiveSet<T> {
+  _read: () => Set<T>;
+  _listen: (controller: LiveSetController<T>) => ?{unsubscribe():void}|()=>void;
+
+  _values: ?Set<T> = null;
+  _activeController: ?LiveSetController<T> = null;
+  _listenCleanup: ?()=>void = null;
+  _ended: boolean = false;
+  _endedWithError: boolean = false;
+  _error: any = null;
+  _queuedCall: boolean = false;
+  _changeQueue: Array<LiveSetChangeRecord<T>> = [];
+  _observers: Array<LiveSetObserver<T>> = [];
+
+  constructor(init: LiveSetInit<T>) {
+    this._read = init.read;
+    this._listen = init.listen;
+  }
+
+  _queueChange(record: ?LiveSetChangeRecord<T>) {
+    if (record) {
+      this._changeQueue.push(record);
+    }
+    if (!this._queuedCall) {
+      this._queuedCall = true;
+      asap(() => {
+        this._queuedCall = false;
+        const changes = this._changeQueue;
+        this._changeQueue = [];
+        let observersToCall;
+        if (this._ended) {
+          observersToCall = this._observers;
+          this._observers = [];
+        } else {
+          observersToCall = this._observers.slice();
+        }
+        observersToCall.forEach(observer => {
+          if (observer.next) {
+            observer.next(changes);
+          }
+          if (this._ended) {
+            if (this._endedWithError) {
+              if (observer.error) observer.error(this._error);
+            } else {
+              if (observer.complete) observer.complete();
+            }
+          }
+        });
+      });
+    }
+  }
+
+  _deactivate() {
+    this._activeController = null;
+    const listenCleanup = this._listenCleanup;
+    if (listenCleanup) {
+      this._listenCleanup = null;
+      listenCleanup();
+    }
+  }
+
+  values(): Set<T> {
+    if (this._values) {
+      return new Set(this._values);
+    } else {
+      return this._read();
+    }
+  }
+
+  isEnded(): boolean {
+    return this._ended;
+  }
+
+  subscribe(observerOrOnNext: LiveSetObserver<T> | (changes: Array<LiveSetChangeRecord<T>>) => void, onError: ?(err: any) => void, onComplete: ?() => void): LiveSetSubscription {
+    const liveSet = this;
+
+    let observer;
+    if (typeof observerOrOnNext === 'function') {
+      observer = {
+        next: observerOrOnNext,
+        error: onError,
+        complete: onComplete
+      };
+    } else {
+      observer = observerOrOnNext;
+    }
+
+    (observer: LiveSetObserver<T>);
+
+    if (this._ended) {
+      const subscription = {
+        closed: false,
+        unsubscribe: () => {
+          subscription.closed = true;
+        }
+      };
+      if (observer.start) {
+        observer.start(subscription);
+      }
+      if (!subscription.closed && observer.complete) {
+        observer.complete();
+      }
+      subscription.closed = true;
+      return subscription;
+    }
+
+    this._observers.push(observer);
+    const subscription = {
+      /*:: closed: false&&` */ get closed() {
+        return liveSet._observers.indexOf(observer) < 0;
+      }/*:: ` */,
+      unsubscribe: () => {
+        const ix = this._observers.indexOf(observer);
+        if (ix >= 0) {
+          this._observers.splice(ix, 1);
+          if (this._observers.length === 0) {
+            this._values = null;
+            this._deactivate();
+          }
+        }
+      }
+    };
+    if (observer.start) {
+      observer.start(subscription);
+    }
+    // Check that they haven't immediately unsubscribed
+    if (this._observers[this._observers.length-1] === observer && !this._activeController) {
+      const values = this._values = this._read();
+      const controller: LiveSetController<T> = this._activeController = {
+        // Flow doesn't support getters and setters yet
+        /*:: closed: false&&` */ get closed() {
+          return liveSet._activeController !== this;
+        }/*:: ` */,
+        add: value => {
+          if (!this._ended && !values.has(value)) {
+            values.add(value);
+            this._queueChange({type: 'add', value});
+          }
+        },
+        remove: value => {
+          if (!this._ended && values.has(value)) {
+            values.delete(value);
+            this._queueChange({type: 'remove', value});
+          }
+        },
+        error: err => {
+          if (this._ended) return;
+          this._ended = true;
+          this._endedWithError = true;
+          this._error = err;
+          this._queueChange();
+          this._deactivate();
+        },
+        end: () => {
+          if (this._ended) return;
+          this._ended = true;
+          this._queueChange();
+          this._deactivate();
+        }
+      };
+      const cleanup = this._listen(controller);
+      if (cleanup != null) {
+        if (typeof cleanup.unsubscribe === 'function') {
+          this._listenCleanup = () => {
+            cleanup.unsubscribe();
+          };
+        } else if (typeof cleanup !== 'function') {
+          throw new TypeError('listen must return null or a function');
+        } else {
+          this._listenCleanup = (cleanup:any);
+        }
+        if (controller.closed) {
+          this._deactivate();
+        }
+      }
+    }
+    return subscription;
+  }
+}
+
+// Assign here because Flow doesn't support computed property keys on classes:
+// https://github.com/facebook/flow/issues/2286
+(LiveSet:any).prototype[$$observable] = function() {
+  return this;
+};
---
+++ src/index.test.js
@@ -0,0 +1,362 @@
+/* @flow */
+
+import LiveSet from '.';
+
+import delay from 'pdelay';
+
+test('read', () => {
+  let currentValue = new Set([5,6,7]);
+  const read = jest.fn(() => currentValue);
+  const ls = new LiveSet({
+    read,
+    listen() {
+      throw new Error('should not be called');
+    }
+  });
+
+  expect(read).toHaveBeenCalledTimes(0);
+  expect(Array.from(ls.values())).toEqual([5,6,7]);
+  expect(read).toHaveBeenCalledTimes(1);
+
+  currentValue = new Set([7,8,9]);
+  expect(Array.from(ls.values())).toEqual([7,8,9]);
+  expect(read).toHaveBeenCalledTimes(2);
+});
+
+test('listen, subscribe', async () => {
+  const unsub = jest.fn();
+  const ls = new LiveSet({
+    read: () => new Set([4,5]),
+    listen(c) {
+      expect(c.closed).toBe(false);
+      c.add(5);
+      c.add(6);
+      c.add(7);
+      let t = setTimeout(() => {
+        expect(c.closed).toBe(false);
+        c.remove(5);
+        c.add(8);
+        t = setTimeout(() => {
+          throw new Error('should not happen');
+        }, 1);
+      }, 1);
+      return () => {
+        expect(c.closed).toBe(true);
+        clearTimeout(t);
+        unsub();
+      };
+    }
+  });
+
+  expect(Array.from(ls.values())).toEqual([4,5]);
+
+  let changeHandlerCallCount = 0;
+  const sub = ls.subscribe(changes => {
+    switch (changeHandlerCallCount++) {
+    case 0:
+      expect(changes).toEqual([{type: 'add', value: 6}, {type: 'add', value: 7}]);
+      expect(Array.from(ls.values())).toEqual([4,5,6,7]);
+      break;
+    case 1:
+      expect(changes).toEqual([{type: 'remove', value: 5}, {type: 'add', value: 8}]);
+      expect(Array.from(ls.values())).toEqual([4,6,7,8]);
+      expect(sub.closed).toBe(false);
+      sub.unsubscribe();
+      expect(sub.closed).toBe(true);
+      break;
+    default:
+      throw new Error(`Should not happen. ${changeHandlerCallCount}`);
+    }
+  });
+  // The listen function should run immediately
+  expect(Array.from(ls.values())).toEqual([4,5,6,7]);
+  // The change handler should be called asynchronously
+  expect(changeHandlerCallCount).toBe(0);
+  expect(unsub).toHaveBeenCalledTimes(0);
+  await delay(40);
+  expect(changeHandlerCallCount).toBe(2);
+  expect(unsub).toHaveBeenCalledTimes(1);
+  expect(ls.isEnded()).toBe(false);
+});
+
+test('subscribe, end', async () => {
+  const listenStart = jest.fn();
+  const unsub = jest.fn();
+  const ls = new LiveSet({
+    read: () => new Set([1]),
+    listen(c) {
+      expect(c.closed).toBe(false);
+      listenStart();
+      c.add(2);
+      let t = setTimeout(() => {
+        expect(c.closed).toBe(false);
+        c.add(3);
+        c.end();
+        expect(c.closed).toBe(true);
+      }, 30);
+      return () => {
+        expect(c.closed).toBe(true);
+        clearTimeout(t);
+        unsub();
+      };
+    }
+  });
+
+  expect(Array.from(ls.values())).toEqual([1]);
+
+  const next = jest.fn();
+  const complete = jest.fn();
+  const sub = ls.subscribe(next, null, complete);
+  expect(next.mock.calls).toEqual([]);
+  expect(Array.from(ls.values())).toEqual([1,2]);
+  await delay(0);
+
+  expect(next.mock.calls).toEqual([
+    [[{type: 'add', value: 2}]]
+  ]);
+  expect(complete).toHaveBeenCalledTimes(0);
+  expect(sub.closed).toBe(false);
+  expect(unsub).toHaveBeenCalledTimes(0);
+  expect(Array.from(ls.values())).toEqual([1,2]);
+  expect(ls.isEnded()).toBe(false);
+
+  await delay(60);
+  expect(next.mock.calls).toEqual([
+    [[{type: 'add', value: 2}]],
+    [[{type: 'add', value: 3}]],
+  ]);
+  expect(complete).toHaveBeenCalledTimes(1);
+  expect(sub.closed).toBe(true);
+  expect(unsub).toHaveBeenCalledTimes(1);
+  expect(ls.isEnded()).toBe(true);
+
+  // Values should be frozen at end time.
+  expect(Array.from(ls.values())).toEqual([1,2,3]);
+
+  {
+    const start = jest.fn();
+    const next = jest.fn();
+    const error = jest.fn();
+    const complete = jest.fn();
+    const sub = ls.subscribe({start, next, error, complete});
+    expect(start).toHaveBeenCalledTimes(1);
+    expect(start.mock.calls[0][0]).toBe(sub);
+    expect(next).toHaveBeenCalledTimes(0);
+    expect(error).toHaveBeenCalledTimes(0);
+    expect(complete).toHaveBeenCalledTimes(1);
+    expect(sub.closed).toBe(true);
+  }
+
+  {
+    const start = jest.fn(sub => {
+      sub.unsubscribe();
+    });
+    const next = jest.fn();
+    const error = jest.fn();
+    const complete = jest.fn();
+    const sub = ls.subscribe({start, next, error, complete});
+    expect(start).toHaveBeenCalledTimes(1);
+    expect(start.mock.calls[0][0]).toEqual(sub);
+    expect(next).toHaveBeenCalledTimes(0);
+    expect(error).toHaveBeenCalledTimes(0);
+    expect(complete).toHaveBeenCalledTimes(0);
+    expect(sub.closed).toBe(true);
+  }
+});
+
+test('error', async () => {
+  const ls = new LiveSet({
+    read: () => new Set([1]),
+    listen(c) {
+      c.add(2);
+      c.error(new Error('foo'));
+    }
+  });
+  const start = jest.fn(), next = jest.fn(), error = jest.fn(), complete = jest.fn();
+  const sub = ls.subscribe({start, next, error, complete});
+  expect(start).toHaveBeenCalledTimes(1);
+  expect(start.mock.calls[0][0]).toEqual(sub);
+  expect(next).toHaveBeenCalledTimes(0);
+  expect(error).toHaveBeenCalledTimes(0);
+  expect(complete).toHaveBeenCalledTimes(0);
+  expect(sub.closed).toBe(false);
+  await delay(0);
+  expect(next.mock.calls).toEqual([
+    [[{type: 'add', value: 2}]]
+  ]);
+  expect(error).toHaveBeenCalledTimes(1);
+  expect(error.mock.calls[0][0].message).toBe('foo');
+  expect(complete).toHaveBeenCalledTimes(0);
+  expect(sub.closed).toBe(true);
+});
+
+test('subscribe, unsubscribe, subscribe', async () => {
+  const listenStart = jest.fn();
+  const unsub = jest.fn();
+  const ls = new LiveSet({
+    read: () => new Set([1]),
+    listen(c) {
+      listenStart();
+      c.add(2);
+      let t = setTimeout(() => {
+        c.add(3);
+      }, 40);
+      return () => {
+        clearTimeout(t);
+        unsub();
+      };
+    }
+  });
+
+  for (let i=0; i<3; i++) {
+    expect(Array.from(ls.values())).toEqual([1]);
+    const changeHandler = jest.fn();
+    const sub = ls.subscribe(changeHandler);
+    expect(changeHandler.mock.calls).toEqual([]);
+    expect(Array.from(ls.values())).toEqual([1,2]);
+    await delay(1);
+    expect(changeHandler.mock.calls).toEqual([
+      [[{type: 'add', value: 2}]]
+    ]);
+    expect(Array.from(ls.values())).toEqual([1,2]);
+    await delay(100);
+    expect(changeHandler.mock.calls).toEqual([
+      [[{type: 'add', value: 2}]],
+      [[{type: 'add', value: 3}]]
+    ]);
+    expect(Array.from(ls.values())).toEqual([1,2,3]);
+    sub.unsubscribe();
+    await delay(0);
+  }
+});
+
+test('multiple subscribers, one immediate unsubscription', async () => {
+  const listenStart = jest.fn();
+  const unsub = jest.fn();
+  const ls = new LiveSet({
+    read: () => new Set([1]),
+    listen(c) {
+      listenStart();
+      c.add(2);
+      let t = setTimeout(() => {
+        c.add(3);
+        t = setTimeout(() => {
+          throw new Error('should not happen');
+        }, 1);
+      }, 1);
+      return () => {
+        clearTimeout(t);
+        unsub();
+      };
+    }
+  });
+
+  expect(Array.from(ls.values())).toEqual([1]);
+  expect(listenStart).toHaveBeenCalledTimes(0);
+  const sub1 = ls.subscribe({});
+  // The listen function should run immediately
+  expect(Array.from(ls.values())).toEqual([1,2]);
+  expect(listenStart).toHaveBeenCalledTimes(1);
+
+  let changeHandlerCallCount = 0;
+  const sub2 = ls.subscribe(changes => {
+    switch (changeHandlerCallCount++) {
+    case 0:
+      expect(changes).toEqual([{type: 'add', value: 2}]);
+      expect(Array.from(ls.values())).toEqual([1,2]);
+      break;
+    case 1:
+      expect(changes).toEqual([{type: 'add', value: 3}]);
+      expect(Array.from(ls.values())).toEqual([1,2,3]);
+      sub2.unsubscribe();
+      break;
+    default:
+      throw new Error(`Should not happen. ${changeHandlerCallCount}`);
+    }
+  });
+
+  sub1.unsubscribe();
+
+  expect(listenStart).toHaveBeenCalledTimes(1);
+  expect(unsub).toHaveBeenCalledTimes(0);
+
+  // The change handler should be called asynchronously
+  expect(changeHandlerCallCount).toBe(0);
+  await delay(40);
+  expect(changeHandlerCallCount).toBe(2);
+  expect(unsub).toHaveBeenCalledTimes(1);
+});
+
+test('multiple subscribers', async () => {
+  const listenStart = jest.fn();
+  const unsub = jest.fn();
+  const ls = new LiveSet({
+    read: () => new Set([1]),
+    listen(c) {
+      listenStart();
+      c.add(2);
+      let t = setTimeout(() => {
+        c.add(3);
+        t = setTimeout(() => {
+          throw new Error('should not happen');
+        }, 1);
+      }, 1);
+      return () => {
+        clearTimeout(t);
+        unsub();
+      };
+    }
+  });
+
+  expect(Array.from(ls.values())).toEqual([1]);
+  expect(listenStart).toHaveBeenCalledTimes(0);
+
+  let changeHandler1CallCount = 0;
+  const sub1 = ls.subscribe(changes => {
+    switch (changeHandler1CallCount++) {
+    case 0:
+      expect(changes).toEqual([{type: 'add', value: 2}]);
+      expect(Array.from(ls.values())).toEqual([1,2]);
+      expect(sub1.closed).toBe(false);
+      sub1.unsubscribe();
+      expect(sub1.closed).toBe(true);
+      break;
+    default:
+      throw new Error(`Should not happen. ${changeHandler1CallCount}`);
+    }
+  });
+
+  // The listen function should run immediately
+  expect(Array.from(ls.values())).toEqual([1,2]);
+  expect(listenStart).toHaveBeenCalledTimes(1);
+
+  let changeHandler2CallCount = 0;
+  const sub2 = ls.subscribe(changes => {
+    switch (changeHandler2CallCount++) {
+    case 0:
+      expect(changes).toEqual([{type: 'add', value: 2}]);
+      expect(Array.from(ls.values())).toEqual([1,2]);
+      break;
+    case 1:
+      expect(changes).toEqual([{type: 'add', value: 3}]);
+      expect(Array.from(ls.values())).toEqual([1,2,3]);
+      expect(sub2.closed).toBe(false);
+      sub2.unsubscribe();
+      expect(sub2.closed).toBe(true);
+      break;
+    default:
+      throw new Error(`Should not happen. ${changeHandler2CallCount}`);
+    }
+  });
+
+  expect(listenStart).toHaveBeenCalledTimes(1);
+  expect(unsub).toHaveBeenCalledTimes(0);
+
+  // The change handlers should be called asynchronously
+  expect(changeHandler1CallCount).toBe(0);
+  expect(changeHandler2CallCount).toBe(0);
+  await delay(40);
+  expect(changeHandler1CallCount).toBe(1);
+  expect(changeHandler2CallCount).toBe(2);
+  expect(unsub).toHaveBeenCalledTimes(1);
+});
