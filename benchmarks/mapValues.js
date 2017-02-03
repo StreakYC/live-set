efb673fbcbe72240fbcfb988e6f85275dc30fd4e
Chris Cowan <agentme49@gmail.com>
2017-02-02 23:05:13 -0800

    Add benchmarks script.

---
+++ benchmarks/mapValues.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env babel-node
+/* @flow */
+/* eslint-disable no-console */
+
+import LiveSet from '../src';
+import map from '../src/map';
+
+let ls = new LiveSet({
+  read: () => new Set([1,2,3,4,5]),
+  listen: () => {}
+});
+
+console.time('map setup');
+for (let i=0; i<200; i++) {
+  ls = map(ls, x => x+1);
+}
+console.timeEnd('map setup');
+
+function read() {
+  return ls.values();
+}
+
+for (let i=0; i<2000; i++) {
+  read();
+}
+
+console.time('read');
+for (let i=0; i<200; i++) {
+  read();
+}
+console.timeEnd('read');
