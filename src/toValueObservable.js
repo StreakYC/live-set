a55c23ccc3bcb0307d8c7ff30bb683bbf6ef8d80
Chris Cowan <agentme49@gmail.com>
2017-02-06 17:21:48 -0800

    Add toValueObservable.

---
+++ src/toValueObservable.js
@@ -0,0 +1,56 @@
+/* @flow */
+
+import LiveSet from '.';
+import Observable from 'zen-observable';
+
+export default function toValueObservable<T>(liveSet: LiveSet<T>) {
+  return new Observable(observer => {
+    const resolvers: Map<T, ()=>void> = new Map();
+
+    function addedItem(value: T) {
+      let resolve;
+      const removal: Promise<void> = new Promise(_resolve => {
+        resolve = _resolve;
+      });
+      resolvers.set(value, (resolve:any));
+      observer.next({value, removal});
+    }
+
+    function removedItem(value: T) {
+      const resolver = resolvers.get(value);
+      if (!resolver) throw new Error('Resolver not found in map for value');
+      resolvers.delete(value);
+      resolver();
+    }
+
+    const sub = liveSet.subscribe({
+      next(changes) {
+        changes.forEach(change => {
+          if (change.type === 'add') {
+            addedItem(change.value);
+          } else if (change.type === 'remove') {
+            removedItem(change.value);
+          }
+        });
+      },
+      error(err) {
+        observer.error(err);
+      },
+      complete() {
+        observer.complete();
+      }
+    });
+
+    for (let value of liveSet.values()) {
+      if (sub.closed) break;
+      addedItem(value);
+    }
+
+    return () => {
+      sub.unsubscribe();
+      resolvers.forEach(resolver => {
+        resolver();
+      });
+    };
+  });
+}
---
+++ src/toValueObservable.test.js
@@ -0,0 +1,64 @@
+/* @flow */
+
+import toValueObservable from './toValueObservable';
+
+import LiveSet from '.';
+import delay from 'pdelay';
+
+test('works', async () => {
+  let controller;
+  const cleanup = jest.fn();
+  const liveSet = new LiveSet({
+    read: () => new Set([5,6]),
+    listen(setValues, _controller) {
+      setValues(this.read());
+      controller = _controller;
+      return cleanup;
+    }
+  });
+
+  const cb = jest.fn();
+
+
+  const sub = toValueObservable(liveSet).subscribe(cb);
+
+  if (!controller) throw new Error();
+  controller.add(7);
+
+  expect(cb.mock.calls.map(x => x[0].value)).toEqual([5,6]);
+  await delay(0);
+  expect(cb.mock.calls.map(x => x[0].value)).toEqual([5,6,7]);
+  controller.add(8);
+  expect(cb.mock.calls.map(x => x[0].value)).toEqual([5,6,7]);
+  await delay(0);
+  expect(cb.mock.calls.map(x => x[0].value)).toEqual([5,6,7,8]);
+
+  const removal6 = jest.fn();
+  cb.mock.calls[1][0].removal.then(removal6);
+
+  controller.remove(6);
+  controller.remove(7);
+
+  await delay(0);
+  expect(removal6.mock.calls).toEqual([[undefined]]);
+
+  const removal7 = jest.fn();
+  cb.mock.calls[2][0].removal.then(removal7);
+
+  await delay(0);
+  expect(removal7.mock.calls).toEqual([[undefined]]);
+
+  expect(sub.closed).toBe(false);
+  expect(cleanup).toHaveBeenCalledTimes(0);
+  sub.unsubscribe();
+  expect(sub.closed).toBe(true);
+  expect(cleanup).toHaveBeenCalledTimes(1);
+
+  expect(cb.mock.calls.map(x => x[0].value)).toEqual([5,6,7,8]);
+
+  const removal8 = jest.fn();
+  cb.mock.calls[3][0].removal.then(removal8);
+
+  await delay(0);
+  expect(removal8.mock.calls).toEqual([[undefined]]);
+});
