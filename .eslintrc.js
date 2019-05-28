b550b11eb97d21c2d3f7a277b16d47e8ffa6afde
Chris Cowan <agentme49@gmail.com>
2019-05-27 18:20:12 -0700

    configure prettier to run on commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,17 +19,10 @@ module.exports = {
     "flowtype/define-flow-type": 1,
     "flowtype/require-valid-file-annotation": ["error", "always"],
 
-    "indent": ["error", 2],
     "linebreak-style": ["error", "unix"],
     "quotes": ["error", "single", "avoid-escape"],
     "semi": ["error", "always"],
     "no-var": ["error"],
     "brace-style": ["error"],
-    "array-bracket-spacing": ["error", "never"],
-    "block-spacing": ["error", "always"],
-    "no-spaced-func": ["error"],
-    "no-whitespace-before-property": ["error"],
-    "space-before-blocks": ["error", "always"],
-    "keyword-spacing": ["error"]
   }
 };
